5d9ead10cd21a69bfba4099c598ed3088e801dc0
Milos Subotic <milos.subotic.sm@gmail.com>
2015-11-23 21:56:03 +0100

    Too much complicating.

--- model/huffman_codec.cpp
+++ model/huffman_codec.cpp
@@ -4,11 +4,62 @@
  */
  
 //////////////////////////////////////////////////////////////////////////////
+// Config.
 
-#include "huffman_codec.h"
+#define BITS
+
+#define LOG_TO_FILE "huffman_codec.log"
 
 //////////////////////////////////////////////////////////////////////////////
 
+#include "huffman_codec.h"
+
+#include <vector>
+
+#if LOG_TO_FILE
+#include <fstream>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+
+int huffman_encode(
+	const uint8_t* data,
+	size_t data_len,
+	uint8_t* compressed,
+	size_t compressed_max_len
+) {
+	if(!data || !compressed){
+		return 1;
+	}
+
+	vector<uint32_t> histogram(256);
+	
+	for(int i = 0; i < data_len; i++){
+		histogram[data[i]]++;
+	}
+	
+#if LOG_TO_FILE
+	ofstream f(LOG_TO_FILE, ios::write);
+	if(!f){
+		return 2;
+	}
+	
+	f << "Histogram:" << endl;
+	for(int i = 0; i
+	
+#endif
+
+	return 0;
+}
+
+int huffman_decode(
+	const uint8_t* compressed,
+	size_t compressed_len,
+	uint8_t* data,
+	size_t data_max_len
+) {
+	return 0;
+}
 
 
 //////////////////////////////////////////////////////////////////////////////
--- model/test_huffman_codec.cpp
+++ model/test_huffman_codec.cpp
@@ -5,11 +5,63 @@
  
 //////////////////////////////////////////////////////////////////////////////
 
-#include <iosteam>
+#include <iostream>
+#include <fstream>
+#include <streambuf>
+#include <vector>
+
+using namespace std;
+
+#include "huffman_codec.h"
 
 //////////////////////////////////////////////////////////////////////////////
 
 int main() {
+	ifstream f("The King's Breakfast.txt");
+	if(!f){
+		cerr << "Cannot open file!" << endl;
+		return 1;
+	}
+	
+	vector<uint8_t> d_in;
+
+	f.seekg(0, ios::end);
+	d_in.reserve(f.tellg());
+	f.seekg(0, ios::beg);
+
+	d_in.assign(
+		istreambuf_iterator<char>(f),
+		istreambuf_iterator<char>()
+	);
+	
+	f.close();
+
+	
+	size_t len = d_in.size();
+	vector<uint8_t> compressed(len*10);
+	vector<uint8_t> d_out(len);
+
+	int rc = huffman_encode(d_in.data(), len, compressed.data(), len*10);
+	if(rc){
+		cerr << "Error while encoding! rc: " << rc << endl;
+		return 1;
+	}
+	
+	rc = huffman_decode(compressed.data(), len*10, d_out.data(), len);
+	if(rc){
+		cerr << "Error while decoding! rc: " << rc << endl;
+		return 1;
+	}
+
+#if 0
+	for(int i = 0; i < len; i++){
+		if(d_in[i] != d_out[i]){
+			cerr << "Mismatch after encoding and decoding at byte index " << i << endl;
+			return 1;
+		}
+	}
+#endif
+
 	return 0;
 }
 
