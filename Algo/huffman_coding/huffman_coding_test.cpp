0970c33bccbad185aa98c961cce7e7744a07c52a
Milos Subotic <milos.subotic.sm@gmail.com>
2016-03-03 20:46:26 +0100

    Some corrections.

--- Algo/huffman_coding/huffman_coding_test.cpp
+++ Algo/huffman_coding/huffman_coding_test.cpp
@@ -22,7 +22,7 @@ using namespace std;
 		cout << #var << " = " << var << endl; \
 	}while(0)
 
-#define DATA_LEN block_len*10
+#define DATA_LEN block_len*2
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -42,6 +42,7 @@ int main() {
 	vector<uint32_t> enc_data;
 	huffman_encode(in_data, enc_data);
 
+	cout << "enc_data size: " << enc_data.size() << endl;
 	cout << "enc_data:" << endl;
 	cout << hex << setfill('0');
 	for(auto iter = enc_data.begin(); iter != enc_data.end(); iter++){
@@ -53,8 +54,8 @@ int main() {
 	huffman_decode(enc_data, out_data);
 
 	cout << "Comparing input and output..." << endl;
-	DEBUG(in_data.size());
-	DEBUG(out_data.size());
+	cout << "in_data size: " << in_data.size() << endl;
+	cout << "out_data size: " << out_data.size() << endl;
 	if(in_data != out_data){
 		cout << "Mismatch!" << endl;
 		for(int i = 0; i < in_data.size(); i++){
