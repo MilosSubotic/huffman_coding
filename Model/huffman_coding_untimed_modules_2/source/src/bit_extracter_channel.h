86270a974ee3342efe52eca769edd939c0c3e215
Milos Subotic <milos.subotic.sm@gmail.com>
2016-03-18 21:45:44 +0100

    Don't make something that don't need for now.

--- Model/huffman_coding_untimed_modules_2/source/src/bit_extracter_channel.h
+++ Model/huffman_coding_untimed_modules_2/source/src/bit_extracter_channel.h
@@ -13,92 +13,64 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 
-// CT is short for Chunk Type.
-// CST is short for Chunk Size Type.
+// BAT is short for Bit Accumulator Type.
+// BAST is short for Bit Accumulator Size Type.
 
-template<typename CT, typename CST>
+template<typename BAT, typename BAST>
 class bit_extracter_server_if : virtual public sc_interface {
 public:
-	virtual void set_chunk_ref(const CT& chunk) = 0;
-	virtual void set_chunk_size_ref(const CST& size) const = 0;
-	/**
-	 * @return If return true need more bits, else client is done.
-	 */
-	virtual bool does_need_more_bits() = 0;
-	virtual void new_chunk_added() = 0;
+	virtual void open_connection(const BAT& chunk, const BAST& size) = 0;
+	enum operation_t {
+		REMOVE_BITS,
+		NEED_MORE_BITS,
+		DONE
+	};
+	virtual operation_t wait_operation() = 0;
 };
 
-template<typename CT, typename CST>
+template<typename BAT, typename BAST>
 class bit_extracter_client_if : virtual public sc_interface {
 public:
-	virtual const CT& get_chunk_ref() const = 0;
-	virtual const CST& get_chunk_size_ref() const = 0;
-	virtual void remove_bits(CST bits_to_remove) = 0;
+	virtual void connect() = 0;
+	virtual const BAT& get_chunk_ref() const = 0;
+	virtual const BAST& get_chunk_size_ref() const = 0;
+	virtual void remove_bits(BAST bits_to_remove) = 0;
 	virtual void need_more_bits() = 0;
 	virtual void done() = 0;
 };
 
 
-template<typename CT, typename CST>
+template<typename BAT, typename BAST>
 class bit_extracter_channel :
 	public sc_channel,
-	public bit_extracter_server_if<CT, CST>,
-	public bit_extracter_client_if<CT, CST> {
+	public bit_extracter_server_if<BAT, BAST>,
+	public bit_extracter_client_if<BAT, BAST> {
 
 public:
 	bit_extracter_channel(sc_module_name name)
-		: sc_channel(name), chunk(0), size {
+		: sc_channel(name), chunk(0) {
 	}
 
-	virtual bool set_chunk(CT chunk) {
-		/*
-		if(have_data){
-			wait(read_event);
-		}
-
-		this->data = data;
-		this->last = last;
-
-		have_data = true;
-		write_event.notify();
-		*/
-
-		wait(client_event);
-	}
-
-	virtual void read(CT& data, bool& last) {
-		/*
-		if(!have_data){
-			wait(write_event);
-		}
-
-		data = this->data;
-		last = this->last;
-
-		have_data = false;
-		read_event.notify();
-		*/
-	}
 
 protected:
 
-	CT chunk;
-	CST size;
+	BAT chunk;
+	BAST size;
 	bool done;
 
 	sc_event client_event;
 };
 
 
-template<typename CT, typename CST>
+template<typename BAT, typename BAST>
 class bit_extracter_server_port :
-	public sc_port<bit_extracter_server_if<CT, CST>> {
+	public sc_port<bit_extracter_server_if<BAT, BAST>> {
 
 };
 
-template<typename CT, typename CST>
+template<typename BAT, typename BAST>
 class bit_extracter_client_port :
-	public sc_port<bit_extracter_client_if<CT, CST>> {
+	public sc_port<bit_extracter_client_if<BAT, BAST>> {
 
 };
 
